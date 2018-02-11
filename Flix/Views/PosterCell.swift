2c05ce0e88c38f96f4d4e779236860db6a2fa1d5
Sandra-Flores <sfloressilverio@csumb.edu>
2018-02-10 20:47:38 -0800

    Collection view setup and layout

---
+++ Flix/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Sandra Flores on 2/8/18.
+//  Copyright © 2018 Sandra Flores. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
